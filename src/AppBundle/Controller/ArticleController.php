dc030551af2de04205a8d491329692415250a657
Juan Luis García Borrego <juanluisgarciaborrego@gmail.com>
2015-11-13 17:05:47 +0100

    article count()

--- src/AppBundle/Controller/ArticleController.php
+++ src/AppBundle/Controller/ArticleController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Article;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -16,6 +17,23 @@ class ArticleController extends Controller
 
         return $this->render('articles/articles.html.twig', array(
             'articles' => $articles
+        ));
+    }
+
+    /**
+     * @Route("/article-{id}", name="listArticle")
+     */
+    public function listArticleAction(Article $article)
+    {
+        $article = $this->getDoctrine()->getRepository('AppBundle:Article')->find(array('id' => $article->getId()));
+
+        $collection = $article->getComments(); // no hace nada
+
+        $totalItems = $collection->count(); //Proboca una consulta COUNT(*)
+
+        return $this->render('articles/article.html.twig', array(
+            'article' => $article,
+            'totalComment' => $totalItems
         ));
     }
 }
