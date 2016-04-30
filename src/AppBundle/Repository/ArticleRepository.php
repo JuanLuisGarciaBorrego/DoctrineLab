a7dfa0f20c0e271c7f1a51da2b734aa123a81c2a
juanluisgarciaborrego <juanluisgarciaborrego@gmail.com>
2016-04-30 16:24:07 +0200

    update date of the findDate repository

--- src/AppBundle/Repository/ArticleRepository.php
+++ src/AppBundle/Repository/ArticleRepository.php
@@ -6,11 +6,11 @@ class ArticleRepository extends \Doctrine\ORM\EntityRepository
 {
     public function findDate()
     {
-        $now = new \DateTime('2016-04-28 00:20:10');
+        $now = new \DateTime('2016-04-26');
 
         return $this->createQueryBuilder('a')
             ->where('DATE(a.datetime) = :date')
-            ->setParameter('date', $now->format('Y-m-d'))
+            ->setParameter('date', $now)
             ->getQuery()
             ->execute();
     }
