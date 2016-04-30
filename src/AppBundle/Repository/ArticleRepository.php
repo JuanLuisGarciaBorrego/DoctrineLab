dc78c49babe388206e9ac2f1dcda03351eec28a9
juanluisgarciaborrego <juanluisgarciaborrego@gmail.com>
2016-04-30 12:01:36 +0200

    DQL functions

--- src/AppBundle/Repository/ArticleRepository.php
+++ src/AppBundle/Repository/ArticleRepository.php
@@ -6,11 +6,11 @@ class ArticleRepository extends \Doctrine\ORM\EntityRepository
 {
     public function findDate()
     {
-        $now = new \DateTime('2016-04-28 10:54:10');
+        $now = new \DateTime('2016-04-28 00:20:10');
 
         return $this->createQueryBuilder('a')
-            ->where('a.datetime = :date')
-            ->setParameter('date', $now)
+            ->where('DATE(a.datetime) = :date')
+            ->setParameter('date', $now->format('Y-m-d'))
             ->getQuery()
             ->execute();
     }
